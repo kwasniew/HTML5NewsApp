b0e454e49a91405bec4c63dd7220470cf8ad2e6b
Thomas Ermesjo <thomas.ermesjo@gmail.com>
2013-02-14 14:22:54 +0100

    Test

--- server/src/com/example/MainResource.java
+++ server/src/com/example/MainResource.java
@@ -5,8 +5,8 @@ import com.googlecode.utterlyidle.annotations.Path;
 
 public class MainResource {
     @GET
-    @Path("/main2")
+    @Path("/main3")
     public String hello() {
-        return "<h2>com.example.Main2 Resource</h2>";
+        return "<h2>com.example.Main3 Resource</h2>";
     }
 }
