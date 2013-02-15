e267658d20e8e159f32f08302c732fc4bb073f3e
krzychukula <krzychukula@gmail.com>
2013-02-15 14:02:31 +0100

    add pubsub lib and use it to display list

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-/*global console:true Downloader:true  */
+/*global console:true Downloader:true PubSub:true */
 ;(function(window, undefined){
     "use strict";
 
@@ -10,11 +10,39 @@
         console.log("Start!");
 
         var title = document.createElement('h1');
-        title.innerText = 'Hello World!';
-        document.body.appendChild(title);
 
         this.downloader = new Downloader();
         this.downloader.start();
+
+        PubSub.subscribe( "downloaded:articles", function(){
+            console.log('downloaded:articles', arguments);
+        } );
+
+        PubSub.subscribe( "downloaded:articles-list", function(name, articles){
+            console.log('downloaded:articles-list', arguments);
+            var entry, img, h2, div;
+            var articlesList = articles.list;
+
+            var len = articlesList.length;
+            for (var i = len - 1; i >= 0; i--) {
+
+                entry = articlesList[i];
+
+                img = new Image();
+                img.onload = entry.img;
+                img.src = entry.img;
+                div = document.createElement('div');
+                h2 = document.createElement('h2');
+                h2.innerHTML = entry.title;
+                div.appendChild(h2);
+                div.appendChild(img);
+
+                document.body.appendChild(div);
+
+            }
+
+        } );
+
 
         window.downloader = this.downloader;
 
