6a6e622793ccf1829ce807a6a2a3ed661a6caf3b
Mateusz Kwasniewski <mateusz.kwasniewski@schibsted.pl>
2013-02-15 13:33:59 +0100

    returning explicit response

--- server/src/com/schibsted/news/NewsResource.java
+++ server/src/com/schibsted/news/NewsResource.java
@@ -1,16 +1,26 @@
 package com.schibsted.news;
 
+import com.googlecode.utterlyidle.MediaType;
+import com.googlecode.utterlyidle.Response;
+import com.googlecode.utterlyidle.ResponseBuilder;
+import com.googlecode.utterlyidle.Status;
 import com.googlecode.utterlyidle.annotations.GET;
 import com.googlecode.utterlyidle.annotations.Path;
-import com.schibsted.Main;
-import com.schibsted.shared.Files;
+
+import static com.googlecode.utterlyidle.MediaType.TEXT_HTML;
+import static com.googlecode.utterlyidle.Status.OK;
+import static com.schibsted.shared.Files.loadTextFileContent;
 
 public class NewsResource {
 
     @GET
     @Path("/")
-    public String mainPage() {
-        return Files.loadTextFileContent("com/schibsted/index.html");
+    public Response mainPage() {
+        return ok("com/schibsted/index.html");
+    }
+
+    private Response ok(String fileName) {
+        return ResponseBuilder.response(OK).contentType(TEXT_HTML).entity(loadTextFileContent(fileName)).build();
     }
 
     @GET
