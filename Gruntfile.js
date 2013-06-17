0eff2b7ea3c8ef775700fa99ca414f69574d6401
Krzysztof Kula <krzychukula@gmail.com>
2013-06-17 13:32:06 +0200

    add gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,118 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['styleguide/lib/<%= pkg.name %>.js'],
+        dest: 'styleguidedist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'styleguidedist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {}
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['styleguide/js/**/*.js', 'styleguide/test/**/*.js']
+      }
+    },
+    qunit: {
+      files: ['styleguide/test/**/*.html']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    },
+    less: {
+      development: {
+        options: {
+          paths: ["stylesheets"]
+        },
+        files: {
+          "styleguidedist/stylesheets/result.css": "styleguide/stylesheets/main.less"
+        }
+      },
+      production: {
+        options: {
+          paths: ["stylesheets"],
+          yuicompress: true
+        },
+        files: {
+          "styleguidedist/stylesheets/result.css": "styleguide/stylesheets/main.less"
+        }
+      }
+    },
+    htmlmin: {                                     // Task
+      dist: {                                      // Target
+        options: {                                 // Target options
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {                                   // Dictionary of files
+          'styleguidedist/index.html': 'styleguide/index.html'
+        }
+      },
+      dev: {                                       // Another target
+        files: {
+          'styleguidedist/index.html': 'styleguide/index.html'
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify', 'less', 'htmlmin']);
+
+};
